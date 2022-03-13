b6bfecb9d7d8a4e056526ca27678836bcaab5fa9
YtaloSauham <ytalo.sauhan@hotmail.com>
2022-03-13 20:54:06 -0300

    useParams

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -14,7 +14,7 @@ export default function MovieCard({movie}: Props) {
             <div className="dsmovie-card-bottom-container">
                 <h3>{movie.title}</h3>
                 <MovieScore />
-                <Link to="/form/1">
+                <Link to={`/form/${movie.id}`}>
                     <div className="btn btn-primary dsmovie-btn">Avaliar</div>
                 </Link>
 
